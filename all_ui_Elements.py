2e7cde9d324713bddc161f6a6ca27bd4f379121a
Multarix <darien2105@gmail.com>
2022-09-10 12:03:37 +1000

    Split into multiple files

---
+++ all_ui_Elements.py
@@ -0,0 +1,197 @@
+
+from __future__ import annotations	# Type hinting
+
+from typehinting import startProgram, miscDataLayout
+
+from PyQt5 import QtGui, QtWidgets
+from PyQt5.QtGui import *
+
+from ui_editPersonWindow import Ui_editPersonWindow as editPersonWindow
+from ui_addRelationWindow import Ui_addRelationWindow as addRelationWindow
+from ui_worldBuildingWindow import Ui_worldBuildingWindow as worldBuildingWindow
+from ui_optionsWindow import Ui_optionsWindow as optionsWindow
+from ui_infoWindow import Ui_creditsWindow as creditsWindow
+
+
+# Fixing python to not be shit
+true = True;
+false = False;
+
+
+class windows():
+	def __init__(this):
+		pass;
+
+	def openCreditsWindow(this, self: startProgram):
+		self.creditsWindow = QtWidgets.QDialog();
+		self.creditsUI = creditsWindow();
+		self.creditsUI.setupUi(self.creditsWindow);
+		self.creditsWindow.show();
+	# End of function
+	
+	
+	def openOptionsWindow(this, self: startProgram):
+		self.optionsWindow = QtWidgets.QDialog();
+		self.optionsUI = optionsWindow();
+		self.optionsUI.setupUi(self.optionsWindow);
+		self.optionsWindow.show();
+	# End of function
+	
+	
+	def openEditCharacterWindow(this, self: startProgram, miscData: miscDataLayout, newChar: bool):
+		"""
+		Function to open the editCharacterUI when pressing the add or edit button on the mainWindowUI
+
+		Args:
+			newChar (bool): Whether a character is new or not
+		"""
+
+		self.editCharacterWindow = QtWidgets.QWidget();
+		self.characterUI = editPersonWindow();
+		self.characterUI.setupUi(self.editCharacterWindow);
+		self.characterUI.characterID.hide(); # People don't need to see this, it's only for data tracking purposes
+		
+		# Fixing Palettes
+		self.characterUI.textEdit.setPalette(miscData["palette"]);
+		self.characterUI.name.setPalette(miscData["palette"]);
+		self.characterUI.species.setPalette(miscData["palette"]);
+		
+		self._characterRelations.clear();
+		
+		# Buttons
+		# Add
+		self.characterUI.addRelation.clicked.connect(lambda: this.openAddRelationWindow(self, miscData, false));
+		
+		# Edit
+		self.characterUI.editRelation.clicked.connect(lambda: this.openAddRelationWindow(self, miscData, true));
+		self.characterUI.relationTable.currentRowChanged.connect(self.unlockEditRemoveRelationBtn)
+		
+		# Remove
+		self.characterUI.removeRelation.clicked.connect(self.removeRelationBtn);
+				
+		# Accept
+		self.characterUI.acceptForm.setDisabled(newChar);
+		self.characterUI.name.textEdited.connect(self.unlockSubmitCharacterBtn);
+		self.characterUI.acceptForm.clicked.connect(lambda: self.acceptCharacterBtn(newChar));
+		
+		# Font
+		self.characterUI.name.setFont(miscData["font"]);
+		self.characterUI.species.setFont(miscData["font"]);
+		self.characterUI.textEdit.setFont(miscData["font"]);
+		self.characterUI.relationTable.setFont(miscData["font"]);
+
+		if(not newChar):
+			currSelected = self.ui.characterList.currentRow();
+			charData = self.data["characters"][currSelected];
+			# id, name, title, age, gender (0=None, 1=Male, 2=Female), species, isdead (0=Alive), information, relations
+			# 0   1     2      3    4                                  5        6                 7            8
+			
+			# ID
+			self.characterUI.characterID.setValue(charData[0]);
+			
+			# Full Name
+			self.characterUI.name.setText(charData[1]);
+			
+			# Title
+			self.characterUI.titleSelector.setCurrentIndex(int(charData[2]));
+			
+			# Age
+			self.characterUI.age.setValue(charData[3]);
+			
+			# Species
+			self.characterUI.species.setText(charData[5]);
+			
+			# Character information
+			self.characterUI.textEdit.setText(charData[7]);
+			
+			# Is Dead
+			if(charData[6] == 1):
+				self.characterUI.dead.setChecked(true);
+			
+			# Gender
+			self.characterUI.genderSelector.setCurrentIndex(charData[4]);
+			
+			# Relations
+			relations: str;
+			relations = charData[8];
+			if(relations):
+				relationsArray = relations.split(", "); # Each relationship is seperated by ", "
+				
+				for relation in relationsArray:
+					rel = relation.split("|"); # Each side of the relation is seperated by a "|"
+					# rel[0] is the person, rel[1] is the type of relationship
+					self._characterRelations.append((int(rel[0]), int(rel[1]))); # Add the item to the internal list, for later
+				
+				self.populateList(self.characterUI.relationTable, "relation");
+		
+		self.editCharacterWindow.show();
+	# End of function
+	
+
+	def openAddRelationWindow(this, self: startProgram, miscData: miscDataLayout, existing: bool):
+		"""
+		Function to open the addRelationUI when pressing the add button on the editCharacterUI
+		"""
+		self.addRelationWindow = QtWidgets.QWidget();
+		self.addRelationUI = addRelationWindow();
+		self.addRelationUI.setupUi(self.addRelationWindow);
+		
+		# Fixing Palettes
+		self.addRelationUI.search.setPalette(miscData["palette"]);
+		self.addRelationUI.searchRelation.setPalette(miscData["palette"]);
+		
+		self.populateList(self.addRelationUI.characterList, "characters");
+	
+		self.addRelationUI.accept.clicked.connect(lambda: self.addRelationToListBtn(existing));
+		self.addRelationUI.characterList.itemSelectionChanged.connect(self.unlockAcceptRelationBtn);
+		self.addRelationUI.relationType.itemSelectionChanged.connect(self.unlockAcceptRelationBtn);
+		
+		# Search bars
+		self.addRelationUI.search.textEdited.connect(lambda: self.searchBar(self.addRelationUI.search, self.addRelationUI.characterList));
+		self.addRelationUI.searchRelation.textEdited.connect(lambda: self.searchBar(self.addRelationUI.searchRelation, self.addRelationUI.relationType));
+		
+		# Font
+		self.addRelationUI.characterList.setFont(miscData["font"]);
+		self.addRelationUI.relationType.setFont(miscData["font"]);
+		self.addRelationUI.search.setFont(miscData["font"]);
+		self.addRelationUI.searchRelation.setFont(miscData["font"]);
+		
+		if(existing): # Existing relationship
+			relationIndex = self.characterUI.relationTable.currentRow();
+			relationship = self._characterRelations[relationIndex];
+			personID = relationship[0];
+			relationTypeIndex = relationship[1];
+			
+			for i in range(len(self.data["characters"])):
+				person = self.data["characters"][i];
+				if(personID == person[0]):
+					self.addRelationUI.characterList.setCurrentRow(i);
+					self.addRelationUI.relationType.setCurrentRow(relationTypeIndex);
+					break;
+
+		self.addRelationWindow.show();
+	# End of function
+	
+	
+	def openWorldBuildingWindow(this, self: startProgram, miscData: miscDataLayout, newDetail: bool):
+		self.worldBuildingWindow = QtWidgets.QDialog();
+		self.worldBuildingUI = worldBuildingWindow();
+		self.worldBuildingUI.setupUi(self.worldBuildingWindow);
+		
+		# Fixing Palettes
+		self.worldBuildingUI.textEditor.setPalette(miscData["palette"]);
+		
+		# Accept button
+		self.worldBuildingUI.accept.setDisabled(newDetail);
+		self.worldBuildingUI.textEditor.textChanged.connect(self.unlockWorldBuildingAcceptBtn);
+		self.worldBuildingUI.accept.clicked.connect(lambda: self.addWorldBuildingToListBtn(newDetail));
+		
+		if(not newDetail):
+			currRow = self.ui.worldBuildingList.currentRow();
+			self.worldBuildingUI.textEditor.setText(self.data["world"][currRow][0]);
+		
+		# Font
+		self.worldBuildingUI.textEditor.setFont(miscData["font"]);
+		
+		self.worldBuildingWindow.show();
+	# End of function
---
+++ fileManagement.py
@@ -0,0 +1,207 @@
+from __future__ import annotations
+from typing import TypedDict
+from typing import Tuple
+
+from typehinting import startProgram, dataLayout
+
+import sqlite3
+import os
+
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import *
+
+# Fixing python to not be shit
+true = True;
+false = False;
+
+
+class fileManager():
+	def __init__(self) -> None:
+		self._version = "1.1";
+		self._fileName = None;
+		self._database = None;
+		
+		self.data: dataLayout;
+		self.data = {
+			"characters": [],
+			"world": []
+		};
+
+	
+	def new(self) -> None:
+		# Gonna just wipe everything, close window and re-open?
+		# Also ask to confirm
+		pass
+	# End of function
+	
+	
+	def _saveAs(self, ui: startProgram) -> bool | str:
+		"""
+		Extra function to save a file with a specific filename
+
+		Returns:
+			bool | str: Returns false if there were no errors, otherwise returns the error code
+		"""
+		# Get the new save location
+		fileLocation = QFileDialog.getSaveFileUrl(ui, "Save File", QtCore.QUrl(""), "Character Tracker (*.chtr)", "Character Tracker (*.chtr)");
+		
+		if(fileLocation[0].toString() != ''): # If the user clicked cancel/ closed the dialog
+			fileLocation = fileLocation[0].toString();
+		else:
+			return 1;
+		
+		fileLocation = fileLocation.replace("file:///", ""); # Remove the file related weirdness from QT
+		fileArray = fileLocation.split("/");
+		fileName = fileArray.pop(); # Get the filename from the last item in the array
+		pathToFile = "/".join(fileArray); # Rejoin the path to file
+		filePath = os.path.join(pathToFile, fileName); # Get the OS's version of the path to the file
+		
+		errorCode = "SAV000"; # Error code for making directory
+		try:
+			if(not os.path.exists(pathToFile)): # If the path to the given directory doesn't exist
+				os.makedirs(pathToFile); # We make the path a thing
+			
+			errorCode = "SAV001"; # Error code for creating/ opening/ saving file
+			file = open(filePath, "w"); # Create or open a file
+			file.write(""); # Overwrite everything in the file to be empty
+			file.close(); # And then save it
+			
+			errorCode = "SQL000"; # Error code for connecting to the SQL
+			self._database = sqlite3.connect(filePath); # Now we turn it into a database
+			
+			errorCode = "SQL001"; # Error code for creating the SQL Schema
+			self._createSchema(); # And create the SQL Schema
+			
+			self._fielName = filePath; # Assuming nothing went wrong, we can now set our fileName variable
+			return false; # Return false, cause there were no errors
+		except:
+			return errorCode; # Something went wrong, so we return the code
+	# End of function
+	
+				
+	def save(self, ui: startProgram, saveAs: bool, data: dataLayout) -> None:
+		"""
+		Function to save a file
+
+		Args:
+			saveAs (bool): Whether or not to call _saveAs()
+			data (dataLayout): The data to save
+		"""
+		
+		if(not self._fileName or saveAs): # If the fileName has not been set, or saveAs is true
+			errorCode = self._saveAs(ui); # We go through 'Save As'
+			
+			if(errorCode): # If there was an error
+				if(errorCode == 1):
+					return;
+				else:
+					print(f"An error occured: {errorCode})");
+					return self._errorMessage(ui, f"An error occured while saving the file (Error code: {errorCode})");
+		
+		print("Saving Changes...");
+		errorCode = "SAV102" # Error code for record deletion
+		self._deleteRecords(ui); # Delete everything from our database
+		
+		errorCode = "SAV103"; # Error code for post deletion, pre character data - if you get this your data is fucked
+		try:
+			for person in data["characters"]:
+				sql = self._database.cursor();
+				sql.execute("INSERT INTO characters (id, name, title, age, gender, species, isdead, information, relationships) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", person);
+				self._database.commit();
+			
+			errorCode = "SAV104"; # Error code for post character data, pre world data
+			for worldItem in data["world"]:
+				sql = self._database.cursor();
+				sql.execute("INSERT INTO worldBuilding (text, notUsed) VALUES (?, ?)", worldItem);
+				self._database.commit();
+			
+			return print("Save Complete!"); # Console message for saving success
+		except:
+			print(f"An error occured: {errorCode})");
+			return self._errorMessage(ui, f"An error occured while saving the file (Error code: {errorCode})");	
+	# End of function
+
+
+	def _deleteRecords(self, ui: startProgram) -> None:
+		try:
+			sql = self._database.cursor();
+			sql.execute("DELETE FROM characters");
+			self._database.commit();
+			
+			sql.execute("DELETE FROM worldBuilding");
+			self._database.commit();
+		except:
+			self._errorMessage(ui, "An error occured while saving the file (Error code: SAV000)"); # Error Code for during deleting data
+	# End of function
+	
+
+	def open(self, ui: startProgram):
+		"""
+		Opens an sqlite database
+
+		Args:
+			file (str): The URL to the file
+		"""
+		fileName = QFileDialog.getOpenFileName(ui, "Choose File", "", "Character Tracker (*.chtr);;All Files (*)")[0];
+		
+		if(not fileName): # If the user clicked cancel/ closed the dialog
+			return;
+		
+		code = "OPN000"; # Not valid SQL
+		try:
+			self._database = sqlite3.connect(fileName);
+			sql = self._database.cursor();
+			
+			code = "OPN001"; # No version table
+			sql.execute("SELECT * from version");
+			versionData = sql.fetchall();
+			if(versionData[0][0] < self._version):
+				self._updateSchema()
+			
+			code = "OPN002"; # No characters table
+			sql.execute("SELECT * FROM characters");
+			self.data["characters"] = sql.fetchall();
+			# self.populateList(self.ui.characterList, "characters"); # This won't work here
+			
+			code = "OPN003"; # No worldbuilding table
+			sql.execute("SELECT * FROM worldBuilding");
+			self.data["world"] =  sql.fetchall();
+			# self.populateList(self.ui.worldBuildingList, "world"); # This won't work here
+			
+			self._fielName = fileName;
+		except:
+			self._errorMessage(ui, f"An error occured while opening the file (Error code: {code}");
+	# End of function
+		
+
+	def _createSchema(self):
+		"""
+		Creates the tables required in the sql spreadsheet
+		"""
+		if(not self._database):
+			return;
+		
+		# Data storage
+		sql = self._database.cursor();
+		sql.execute('CREATE TABLE "characters" ("id" INTEGER, "name" TEXT, "title" INTEGER, "age" INTEGER, "gender" INTEGER, "species" TEXT, "isdead" INTEGER, "information" TEXT, "relationships" TEXT)');
+		self._database.commit();
+		
+		sql.execute('CREATE TABLE "worldBuilding" ("text" TEXT, "notUsed" INTEGER);');
+		self._database.commit();
+		
+		# Versioning control system
+		sql.execute('CREATE TABLE "version" ("currentVersion TEXT, "notUsed", INTEGER)');
+		self._database.commit();
+		sql.execute('INSERT INTO version (currentVersion, notUsed) VALUES (?, ?)', (self._version, 0));
+		self._database.commit();
+	# End of function
+	
+
+	def _updateSchema(self):
+		pass;
+	# End of function
+
+
+	def _errorMessage(self, ui: startProgram, message: str):
+		QMessageBox.critical(ui, "Error", message);
+	# End of function
